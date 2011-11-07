14baaeac4425246060cc15dd876830b90b3fef4a
Playtomic <ben@playtomic.com>
2011-11-07 11:07:05 -0300

    Removed debugging line

--- TestUnity/Assets/Plugins/Playtomic/Playtomic_Request.cs
+++ TestUnity/Assets/Plugins/Playtomic/Playtomic_Request.cs
@@ -27,7 +27,7 @@ internal class Playtomic_Request
 		var turl = URLStub + "tracker/q.aspx?q=" + data + "&swfid=" + Playtomic.GameId + "&url=" + Escape(Playtomic.SourceUrl) + "&" + r.Next(1000000) + "Z";
 		
 		
-		turl = "http://g7bf0867af8a64758.api.playtomic.com/tracker/q.aspx?q=p/1~pls/Tutorial+1+targets~lc/Starts/Tutorial+1+targets~plw/Tutorial+1+targets~lc/Completed/Tutorial+1+targets~la/Elapsed+Time/Tutorial+1+targets/16.9799880981445&swfid=4526&url=http%3A%2F%2Flocalhost%2F&508820Z";
+		//turl = "http://g7bf0867af8a64758.api.playtomic.com/tracker/q.aspx?q=p/1~pls/Tutorial+1+targets~lc/Starts/Tutorial+1+targets~plw/Tutorial+1+targets~lc/Completed/Tutorial+1+targets~la/Elapsed+Time/Tutorial+1+targets/16.9799880981445&swfid=4526&url=http%3A%2F%2Flocalhost%2F&508820Z";
 		Debug.Log("Sending data to " + turl);
 		
 		WWW www = new WWW(turl, post);
